4d293a0da456af092e524d3d4340be360e5a443a
Kamanati <98698457+Kamanati@users.noreply.github.com>
2023-04-29 18:37:42 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anime-hive',
+    version='0.1.0',
+    description='Simple Tool To get All anime Details No Mater the circumstances Grap each and every details and has Filter Options',
+    author='HasanFq',
+    author_email='hasanfq818@mail.com',
+    url='https://github.com/Kamanati/Anime-Hive',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'pyfiglet',
+        'termcolor',
+    ],
+)
+
