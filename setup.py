7015797df2b1702c242f0ddec4d953e9e779b51f
Kamanati <98698457+Kamanati@users.noreply.github.com>
2023-04-29 20:09:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
-
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
 setup(
-    name='anime-hive',
-    version='0.1.0',
-    description='Simple Tool To get All anime Details No Mater the circumstances Grap each and every details and has Filter Options',
-    author='HasanFq',
-    author_email='hasanfq818@mail.com',
-    url='https://github.com/Kamanati/Anime-Hive',
-    packages=find_packages(),
-    install_requires=[
-        'bs4',
-        'pyfiglet',
-        'termcolor',
-   
-    entry_points={
-        'console_scripts': [
-            'anime-hive = main:main'
-
-    ],
+  name='Anime-Hive',
+  version='0.0.1',
+  description='Simple Tool To get All anime Details No Mater the circumstances Grap each and every details and has Filter Options',
+  url='',  
+  author='hasanfq',
+  author_email='hasanfq818@mail.com,
+  license='MIT', 
+  classifiers=classifiers,
+  
+  packages=find_packages(),
+  install_requires=['bs4,pyfiglet,termcolor'] 
 )
-
